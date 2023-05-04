dc0ad2d0394c8d55b015f2b34e461234ba6543ab
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-05-04 22:35:21 +0300

    added:svg

--- AwesomeProject/Screens/RegistrationScreen/RegistrationScreen.js
+++ AwesomeProject/Screens/RegistrationScreen/RegistrationScreen.js
@@ -4,13 +4,23 @@ import {
   TextInput,
   TouchableOpacity,
   Text,
+  Image,
 } from "react-native";
 
 export default RegistrationScreen = () => {
   return (
     <View style={styles.form}>
+      <View style={styles.box}>
+        <Image style={styles.avatar} />
+        <View style={styles.boxAddAvatar}>
+          <Image
+            source={require("../../assets/add.svg")}
+            style={styles.addAvatar}
+          />
+        </View>
+      </View>
       <View style={styles.formContainer}>
-        <Text style={styles.text}>Регистрация</Text>
+        <Text style={styles.title}>Регистрация</Text>
         <View>
           <TextInput style={styles.input} placeholder="Логин"></TextInput>
         </View>
@@ -47,11 +57,38 @@ const styles = StyleSheet.create({
   formContainer: {
     marginHorizontal: 16,
   },
-  text: {
+  boxAddAvatar: {
+    position: 'absolute',
+    bottom: 14,
+    right: -14,
+
+    backgroundColor: '#FFFFFF',
+
+    width: 25,
+    height: 25,
+
+    borderWidth: 1,
+    borderColor: '#FF6C00',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    position: "absolute",
+    top: -60,
+    backgroundColor: "#F6F6F6",
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+  },
+ 
+
+  title: {
     color: "#212121",
     fontSize: 30,
     fontWeight: 500,
-    marginTop:92,
+    marginTop: 92,
   },
   input: {
     borderWidth: 1,
@@ -73,7 +110,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   textSingUp: {
-   
     color: "#1B4371",
     fontSize: 16,
     fontWeight: 400,
